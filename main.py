16b4fa35195efaf147c4bb5d4bc0d9f6119b656f
nvstefan <nvstefan@mail.ru>
2024-08-17 09:56:02 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+number_of_completed_tasks = "всего задач 12,"
+number_of_hours_spent = "затрачено часов: 1.5,"
+Course_name = "Python,"
+print(Course_name ,number_of_completed_tasks ,number_of_hours_spent , 'среднее время выполнения' ,1.5 / 12 ,"часа.")
